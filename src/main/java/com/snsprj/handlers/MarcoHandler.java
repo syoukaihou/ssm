ac0e889f792ed633b0c51b3a3377dc8c4d88056a
DESKTOP-71QBI00\john <syoukaihou@gmail.com>
2017-04-28 18:30:11 +0800

    测试websocket

---
+++ src/main/java/com/snsprj/handlers/MarcoHandler.java
@@ -0,0 +1,14 @@
+package com.snsprj.handlers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.socket.handler.AbstractWebSocketHandler;
+
+public class MarcoHandler extends AbstractWebSocketHandler{
+
+	private static final Logger logger = LoggerFactory.getLogger(MarcoHandler.class);
+	
+	protected void handleTextMessage(){
+		
+	}
+}
