3e9cc73572b50fd2aa9707ee14f73f391f5f5f26
HOME\John <syoukaihou@gmail.com>
2017-06-07 00:33:34 +0800

    测试接口注入

--- src/main/java/com/snsprj/controller/IndexController.java
+++ src/main/java/com/snsprj/controller/IndexController.java
@@ -2,19 +2,23 @@ package com.snsprj.controller;
 
 import com.snsprj.service.IUserAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  * Created by skh on 2017/6/6.
  */
+@Controller
 public class IndexController {
 
     @Autowired
-    IUserAuthService userAuthService;
+    IUserAuthService iUserAuthService;
 
 
+    @RequestMapping(value = "/index")
     public String login(){
 
-        Boolean result = userAuthService.login("123","123456");
+        Boolean result = iUserAuthService.login("123","123456");
 
         return "" + result;
     }
--- src/main/java/com/snsprj/service/UserAuthServiceImpl.java
+++ src/main/java/com/snsprj/service/UserAuthServiceImpl.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Service;
 import com.snsprj.dao.UserMapper;
 import com.snsprj.dto.User;
 
-@Service
+@Service("iUserAuthService")
 public class UserAuthServiceImpl implements IUserAuthService {
 
 	@Autowired
-	UserMapper userMapper;
+	private UserMapper userMapper;
 	
 	@Override
 	public boolean login(String account, String password) {
