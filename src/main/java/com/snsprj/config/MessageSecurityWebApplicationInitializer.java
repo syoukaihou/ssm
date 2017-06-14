1a32f08828f392dda303f4eb28916a181ebbb0ed
skh <syoukaihou@gmail.com>
2017-06-14 16:02:37 +0800

    测试security

---
+++ src/main/java/com/snsprj/config/MessageSecurityWebApplicationInitializer.java
@@ -0,0 +1,9 @@
+package com.snsprj.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/**
+ * Created by skh on 2017/6/14.
+ */
+public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
