59d244ad841cc0f34c48b89a4336366f48923351
skh <syoukaihou@gmail.com>
2017-06-14 18:31:44 +0800

    初步security

--- src/main/java/com/snsprj/config/MessageSecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.snsprj.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by skh on 2017/6/14.
- */
-public class MessageSecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
