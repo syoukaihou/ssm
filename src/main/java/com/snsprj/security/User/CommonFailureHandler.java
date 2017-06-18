12df8a8e28406f1abbcfd220f8b87a197530f6e3
HOME\John <syoukaihou@gmail.com>
2017-06-18 22:44:12 +0800

    ss

---
+++ src/main/java/com/snsprj/security/User/CommonFailureHandler.java
@@ -0,0 +1,11 @@
+package com.snsprj.security.User;
+
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+/**
+ * Created by John on 2017/6/18.
+ */
+public class CommonFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+
+}
--- src/main/java/com/snsprj/security/User/CommonSuccessHandler.java
+++ src/main/java/com/snsprj/security/User/CommonSuccessHandler.java
@@ -1,7 +1,9 @@
 package com.snsprj.security.User;
 
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
 /**
  * Created by John on 2017/6/15.
  */
-public class CommonSuccessHandler {
+public class CommonSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 }
