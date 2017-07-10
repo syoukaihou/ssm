4792fbdd234da77b4d1fc27399125257ed5a772c
skh <syoukaihou@gmail.com>
2017-07-10 18:23:10 +0800

    尚存在注解的定时任务不执行的问题

---
+++ src/main/java/com/snsprj/quartz/TestTask.java
@@ -0,0 +1,17 @@
+package com.snsprj.quartz;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by skh on 2017/7/10.
+ */
+@Component
+public class TestTask {
+
+    // 秒 分 小时	 日期 月份 星期(日期和星期互斥，必须有一个设置为"?")
+    @Scheduled(cron = "* 0/1 * * * *")
+    public void doTask(){
+        System.out.println("基于注解的spring定时任务正在执行...");
+    }
+}
