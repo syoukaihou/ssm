eae582f0dbd3023db2ee56bdcab4f9e8dba8a6cb
syoukaihou <syoukaihou@gmail.com>
2017-09-10 21:56:23 +0800

    分页获取AD数据

--- src/main/java/com/snsprj/service/impl/ADDataSyncImpl.java
+++ src/main/java/com/snsprj/service/impl/ADDataSyncImpl.java
@@ -49,12 +49,16 @@ public class ADDataSyncImpl implements IADDataSync {
         }
     }
 
-
+    /**
+     * 分页获取所有部门信息
+     * @param ctx LdapContext
+     * @param adDn String
+     */
     public void getAllDept(LdapContext ctx,String adDn){
 
         String searchFilter = "(objectClass=organizationalUnit)";
         // 指定AD域用户返回属性
-        String[] attrs={"uSNCreated","objectGUID","distinguishedName","description"};
+        String[] attrs={"distinguishedName", "objectGUID", "whenCreated", "whenChanged"};
 
         // 构建查询控制器
         SearchControls searchCtls = new SearchControls();
@@ -78,10 +82,12 @@ public class ADDataSyncImpl implements IADDataSync {
 
                     SearchResult sr = (SearchResult) results.next();
                     String distinguishedName = sr.getAttributes().get("distinguishedName").get().toString().trim();
-                    String createAt = sr.getAttributes().get("uSNCreated").get().toString().trim();
+                    String createdAt = sr.getAttributes().get("whenCreated").get().toString().trim();
+                    String updatedAt = sr.getAttributes().get("whenChanged").get().toString().trim();
                     String guid = sr.getAttributes().get("objectGUID").get().toString();
                     System.out.println(distinguishedName);
-                    System.out.println(createAt);
+                    System.out.println("createdAt:" + createdAt);
+                    System.out.println("updatedAt:" + updatedAt);
                     System.out.println(guid);
                 }
 
@@ -91,7 +97,60 @@ public class ADDataSyncImpl implements IADDataSync {
         }catch (IOException | NamingException ex ){
             ex.printStackTrace();
         }
+    }
 
+
+
+    public void getAllUser(LdapContext ctx,String adDn){
+
+        String searchFilter = "(objectClass=User)";
+
+        // 指定AD域用户返回属性
+        String[] returnedAtts = {"objectGUID", "sAMAccountName", "mail", "telephoneNumber",
+                "displayName", "whenCreated", "whenChanged"};
+        int pageSize=100;
+        byte[] cookie = null;
+
+        SearchControls searchCtls = new SearchControls();
+        searchCtls.setSearchScope(SearchControls.SUBTREE_SCOPE);
+        searchCtls.setReturningAttributes(returnedAtts);
+
+        try {
+            ctx.setRequestControls((new Control[] { new PagedResultsControl(  pageSize, Control.CRITICAL) }) );
+
+            do {
+                NamingEnumeration<SearchResult> results = ctx.search(adDn, searchFilter, searchCtls);
+
+                while(null!=results && results.hasMoreElements()) {
+                    SearchResult sr = results.next();
+
+                    String guid = sr.getAttributes().get("objectGUID").get().toString();
+                    String sAMAccountName = sr.getAttributes().get("sAMAccountName").get().toString();
+//                    String mail = sr.getAttributes().get("mail").get().toString();
+//                    String telephoneNumber = sr.getAttributes().get("telephoneNumber").get().toString();
+                    String displayName = sr.getAttributes().get("displayName").get().toString().trim();
+                    String createdAt = sr.getAttributes().get("whenCreated").get().toString().trim();
+                    String updatedAt = sr.getAttributes().get("whenChanged").get().toString().trim();
+
+                    System.out.println(guid);
+                    System.out.println(sAMAccountName);
+//                    System.out.println(mail);
+//                    System.out.println(telephoneNumber);
+                    System.out.println(displayName);
+                    System.out.println("createdAt:" + createdAt);
+                    System.out.println("updatedAt:" + updatedAt);
+
+
+                }
+
+                cookie = parseControls(ctx.getResponseControls());
+                ctx.setRequestControls(new Control[] { new PagedResultsControl(  pageSize, cookie, Control.CRITICAL) });
+            }while ((cookie != null) && (cookie.length != 0));
+
+
+        }catch (IOException | NamingException ex){
+            ex.printStackTrace();
+        }
     }
 
     private static byte[] parseControls(Control[] controls) throws NamingException {
@@ -111,13 +170,15 @@ public class ADDataSyncImpl implements IADDataSync {
         String adUrl = "ldap://192.168.1.237";
         String username = "tongxy@snsprj.cn";
         String password = "Tongxiangyu88";
+        String adDn = "OU=同福客栈酒店管理有限公司,DC=snsprj,DC=cn";
 
         ADDataSyncImpl aDDataSyncImpl = new ADDataSyncImpl();
 
         LdapContext ctx = aDDataSyncImpl.adLogin(adUrl,username,password);
 
-        String adDn = "OU=同福客栈酒店管理有限公司,DC=snsprj,DC=cn";
-        aDDataSyncImpl.getAllDept(ctx,adDn);
+
+//        aDDataSyncImpl.getAllDept(ctx,adDn);
+        aDDataSyncImpl.getAllUser(ctx,adDn);
     }
 
 
