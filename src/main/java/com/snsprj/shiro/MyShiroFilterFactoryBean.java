3bc2f66eb1dd591225ef9417d7d02a3cde8a6d12
xiaohb <xiaohb0802@thundersoft.com>
2017-11-02 14:16:57 +0800

    shiro动态权限分配

---
+++ src/main/java/com/snsprj/shiro/MyShiroFilterFactoryBean.java
@@ -0,0 +1,15 @@
+package com.snsprj.shiro;
+
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+
+public class MyShiroFilterFactoryBean extends ShiroFilterFactoryBean{
+
+	@Override
+	public void setFilterChainDefinitions(String definitions) {
+
+	}
+
+	
+	
+	
+}
