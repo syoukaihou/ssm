fd6fdf0287418fcfd428722d8a535318942e22af
xiaohb <xiaohb0802@thundersoft.com>
2017-11-06 11:50:16 +0800

    使用单例模式发送邮件

---
+++ src/main/java/com/snsprj/utils/MyJavaMailSender.java
@@ -0,0 +1,24 @@
+package com.snsprj.utils;
+
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+public class MyJavaMailSender extends JavaMailSenderImpl{
+
+	private static MyJavaMailSender instance;
+	
+	private MyJavaMailSender(){
+		
+	}
+	
+	public static MyJavaMailSender getInstance() {
+		if (instance == null) {
+			synchronized (MyJavaMailSender.class) {
+				if (instance == null) {
+					instance = new MyJavaMailSender();
+				}
+			}
+		}
+		return instance;
+	}
+	
+}
