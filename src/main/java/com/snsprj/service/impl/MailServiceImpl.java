576ce4e0160b8b4bd94399b59fe08fd644201987
xiaohb <xiaohb0802@thundersoft.com>
2017-11-06 12:27:06 +0800

    调试QQ邮箱发送邮件

--- src/main/java/com/snsprj/service/impl/MailServiceImpl.java
+++ src/main/java/com/snsprj/service/impl/MailServiceImpl.java
@@ -1,29 +1,35 @@
 package com.snsprj.service.impl;
 
+import java.util.Properties;
+
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.snsprj.service.IMailService;
+import com.snsprj.utils.MyJavaMailSender;
 
 @Service("iMailService")
 public class MailServiceImpl implements IMailService{
-
-
-	@Autowired
-	JavaMailSender javaMailSender;
 	
 	
 	public void htmlMail(String[] receiver, String subject, String html){
+		
+		// TODO 从数据库中拿到邮件的配置信息
+		String username = "xxx@163.com";
+		String password = "xxxxxx";
+		String sender = "xxx@163.com";
+		String host = "smtp.163.com";
+		Integer port = 25;
+		
+		MyJavaMailSender javaMailSender = this.initMailSender(host, port, username, password);
 		MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 		MimeMessageHelper messageHelper;
         try {
 	        messageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
-	        messageHelper.setFrom("sy0ukaih0u@163.com");
+	        messageHelper.setFrom(sender);
 	        messageHelper.setTo(receiver);
 
 	        messageHelper.setSubject(subject);
@@ -33,6 +39,37 @@ public class MailServiceImpl implements IMailService{
         } catch (MessagingException e) {
 	        e.printStackTrace();
         }
+	}
+	
+	/**
+	 * 
+	 * @Author skh
+	 * @Date 2017年11月6日
+	 *
+	 * @param host 邮件服务器地址
+	 * @param port 邮件服务器端口
+	 * @param username 邮件用户名
+	 * @param password 邮件密码
+	 * @return
+	 */
+	private MyJavaMailSender initMailSender(String host, Integer port, String username, String password){
+
+		MyJavaMailSender mailSender = MyJavaMailSender.getInstance();
+		
+		mailSender.setHost(host);
+		mailSender.setPort(port);
+		mailSender.setUsername(username);
+		mailSender.setPassword(password);
+		
+		Properties properties = new Properties();
+		properties.setProperty("mail.smtp.auth", "true");
+		properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.smtp.ssl.enable", "true");
+        
+		mailSender.setJavaMailProperties(properties);
+		
+		return mailSender;
 	}
     
 }
