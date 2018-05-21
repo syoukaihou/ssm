9e8ef18450519951e205b722a081fd6f8f42142c
MAC_SKH <mac_skh@gmail.com>
2018-05-21 18:54:45 +0800

    kafka consumer

--- src/main/java/com/snsprj/kafka/MyKafKaConsumer.java
+++ src/main/java/com/snsprj/kafka/MyKafKaConsumer.java
@@ -1,11 +1,20 @@
 package com.snsprj.kafka;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.listener.MessageListener;
+
 /**
  * @author SKH
  * @program ssm
  * @description
  * @date 2018-05-21 16:09
  **/
-public class MyKafKaConsumer {
+public class MyKafKaConsumer implements MessageListener<Integer, String> {
+
+    @Override
+    public void onMessage(ConsumerRecord<Integer, String> record) {
+
+        System.out.println("获取kafka消息===>" + record);
+    }
 
 }
